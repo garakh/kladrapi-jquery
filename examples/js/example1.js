280c0d644a2be0490c7e6ce9fb461a5b42b382ea
xescoder <xescoder@gmail.ru>
2013-08-06 11:56:35 +0400

    edit examples
    Signed-off-by: xescoder <xescoder@gmail.ru>

--- examples/js/example1.js
+++ examples/js/example1.js
@@ -1,18 +1,11 @@
 (function($){
     $(function() {
-        $( '[name="location"]' ).kladr({
-            token: '51dfe5d42fb2b43e3300006e',
-            key: '86a2c2a06f1b2451a87d05512cc2c3edfdf41969',
-            type: $.ui.kladrObjectType.CITY,
-            select: function( event, ui ) {
-                $( '[name="street"]' ).kladr( 'option', { parentType: $.ui.kladrObjectType.CITY, parentId: ui.item.obj.id } );
-            }
-        });
-
         $( '[name="street"]' ).kladr({
             token: '51dfe5d42fb2b43e3300006e',
             key: '86a2c2a06f1b2451a87d05512cc2c3edfdf41969',
-            type: $.ui.kladrObjectType.STREET
+            type: $.ui.kladrObjectType.STREET,
+            parentType: $.ui.kladrObjectType.CITY,
+            parentId: '7700000000000'
         });
     });
 })(jQuery);
